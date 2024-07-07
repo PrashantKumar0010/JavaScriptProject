9ef54deb746d2422a1f83bb5676ce555b69b0bd3
PRASHANTKUAMR <rdxprashant9876@gmail.com>
2024-07-07 11:09:26 +0530

    Unlimited color changer

---
+++ unlimitedColor/script.js
@@ -0,0 +1,37 @@
+const Start = document.querySelector('.start')
+const Stop = document.querySelector('.stop')
+GetColorCode = () => {
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
+}
+
+// method 1
+
+
+// let startMe
+// Start.addEventListener('click', () => {
+//      startMe = setInterval(StartEvent, 1000);
+// })
+// const StartEvent = () => {
+//     document.querySelector('body').style.backgroundColor = `${GetColorCode()}`
+// }
+// Stop.addEventListener('click', ()=> clearInterval(startMe))
+
+
+// method 2
+
+
+let StartMe
+ChangeColor = () => {
+    StartMe = setInterval(ChangingColor, 1000);
+
+    function ChangingColor() {
+        const ColorCode = GetColorCode()
+        document.querySelector('body').style.backgroundColor = `${ColorCode}`
+        const code = document.querySelector('.colorCode').innerHTML = `${ColorCode}`
+    }
+}
+StopChangeColor = () => clearInterval(StartMe)
+
+Start.addEventListener('click', () => ChangeColor())
+Stop.addEventListener('click', () => StopChangeColor())
